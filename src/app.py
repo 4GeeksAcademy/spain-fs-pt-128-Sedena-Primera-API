63ca87029bb2e2167ebddd7dc5307125d20f5da9
Pablo Serrano García <disciplineart17@gmail.com>
2026-03-01 12:03:08 +0000

    TErminada mi api

---
+++ src/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify,request
+
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_text = jsonify(todos)
+    
+    return json_text
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.get_json(force=True)
+
+
+    required_fields = ["label", "done"]
+
+
+    missing = [field for field in required_fields if field not in request_body]  
+   
+    if missing:
+        return jsonify({"error": f"Missing fields: {missing}"}), 404
+
+
+    todos.append(request_body)
+    return jsonify(todos), 200
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    if position < 0 or position >= len(todos):
+        return jsonify({"error": "Índice fuera de los existentes"}), 400
+
+    todos.pop(position)
+    return jsonify(todos), 200
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
